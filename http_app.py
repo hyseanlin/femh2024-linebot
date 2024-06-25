b23db2967f39979f9ab17111996851d47ed09c60
hyseanlin <hylin827@gmail.com>
2024-06-25 18:46:00 +0800

    no message

--- http_app.py
+++ http_app.py
@@ -3,12 +3,12 @@ import streamlit as st
 from urllib.parse import parse_qs
 
 # Get request example
-query_params = st.experimental_get_query_params()
+query_params = st.query_params()
 if 'name' in query_params:
     st.write(f"Hello, {query_params['name'][0]}!")
 
 # Post request example
-if 'POST' in st.experimental_get_query_params():
+if 'POST' in st.query_params():
     st.write("POST Request detected!")
     if 'data' in st.experimental_get_query_params():
         st.write(f"Data: {st.experimental_get_query_params()['data'][0]}")
